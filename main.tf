80b603562309cf01146f30122a8ac90a03c08065
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2023-12-17 20:32:55 +0530

    fix tf format issues

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/24"
   tags = {
-    "Name":"main"
-    "Managed_By":"terraform_v1.5.7"
+    "Name" : "main"
+    "Managed_By" : "terraform_v1.5.7"
   }
 }
 
--- out.tf
+++ out.tf
@@ -1,5 +1,5 @@
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
   description = "VPC id of the VPC"
 }
